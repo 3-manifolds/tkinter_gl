4a2214382f3f55e1703b83b86668a74c8526618a
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2024-01-30 16:02:15 -0800

    More work on the square test example.

--- test/square.py
+++ test/square.py
@@ -5,6 +5,8 @@ from OpenGL import GL
 
 import time
 
+separate_slider = True
+
 class GLView(GLViewBase):
     def __init__(self, parent):
         super().__init__(parent)
@@ -15,8 +17,10 @@ class GLView(GLViewBase):
 
     def set_size(self, value):
         self.size = float(value)
+        self.draw()
 
     def draw(self, event=None):
+ 
         GL.glViewport(0, 0, self.winfo_width(), self.winfo_height())
 
         GL.glClearColor(0, 0, 0, 1)
@@ -48,13 +52,14 @@ class Window(tkinter.Tk):
         self.label.grid(row=0, column=0)
 
         self.glView = GLView(self)
-        self.glView.grid(row=2, column=0)
+        self.glView.grid(row=1, column=0)
 
-        self.slider = ttk.Scale(master=self,
-                                orient=tkinter.HORIZONTAL,
-                                command=self.glView.set_size,
-                                value=self.glView.size)
-        self.slider.grid(row=1, column=0, sticky=tkinter.NSEW)
+        if not separate_slider:
+            self.slider = ttk.Scale(master=self,
+                                    orient=tkinter.HORIZONTAL,
+                                    command=self.glView.set_size,
+                                    value=self.glView.size)
+            self.slider.grid(row=1, column=0, sticky=tkinter.NSEW)
 
         self.bind('<KeyPress>', self.tkKeyPress)
         self.bind('<KeyRelease>', self.tkKeyRelease)
@@ -91,5 +96,17 @@ class Window(tkinter.Tk):
         self.after(5, self.advance)
 
 if __name__ == '__main__':
-    Window().mainloop()
+
+    window = Window()
+
+    if separate_slider:
+        sliderWindow = tkinter.Tk()
+        slider = ttk.Scale(master=sliderWindow,
+                           orient=tkinter.HORIZONTAL,
+                           command=window.glView.set_size)
+        slider.grid(row=0, column=0, sticky=tkinter.NSEW)
+
+    print("Using OpenGL", window.square_widget.gl_version())
+
+    window.mainloop()
 
